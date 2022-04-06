ad5f8cc1917813a592994abe30d307035f0be374
usmarcv <usmarc.v@gmail.com>
2022-04-06 09:29:52 -0300

    Stable Marriage Problem

---
+++ SCC5900/1 - Stable Marriage Problem/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+typedef long long ll;
+
+using namespace std;
+
+int main(){_
+
+
+	int teste;
+
+  return 0;
+}
+
+
