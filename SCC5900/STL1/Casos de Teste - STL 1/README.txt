64e5caf40089fae9dd9a3cf6848cae0e47c57ecc
usmarcv <usmarc.v@gmail.com>
2022-04-21 12:09:36 -0300

    Exercício STL1

---
+++ SCC5900/STL1/Casos de Teste - STL 1/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 12/04/2022 10:10:09 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: STL 1
---
+++ SCC5900/STL3/11239.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+
+def main():
+    projects, students = {}, defaultdict(set)
+    project_get = projects.get
+    proj = None
+
+    while 1:
+        data = input().strip()
+        if data == '0':
+            break
+        elif data != '1':
+            if data.isupper():
+                # project, the same project may show many times
+                proj = data
+                projects[proj] = project_get(proj, 0)
+            else:
+                # student
+                students[data].add(proj)
+        else:
+            # end of test cases
+            for k, project_set in students.items():
+                if len(project_set) == 1:
+                    for name in project_set:
+                        projects[name] += 1
+
+            # sort by value first (descending) then by project name
+            sorted_data = sorted(projects.items(), key=lambda k: (-k[1], k[0]))
+            for name, cnt in sorted_data:
+                print(name, cnt)
+            projects.clear()
+            students.clear()
+
+
+if __name__ == '__main__':
+    main()
