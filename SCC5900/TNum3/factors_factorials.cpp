12f117431b9e64c5d07f16d765d8c81920443f13
Márcus Vinícius Lobo Costa <usmarc.v@gmail.com>
2022-06-13 12:44:49 -0300

    TNum3

--- SCC5900/TNum3/factors_factorials.cpp
+++ SCC5900/TNum3/factors_factorials.cpp
@@ -20,14 +20,12 @@ int isPrime(int k){
 
 int main(){_
 
-	int primes[26]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+	int primes[26] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 	
 	int n;
 
-	while(cin >> n){
+	while(cin >> n && n){
 		
-		if(n == 0) break;
-
 		int factor[25] = {0};
 		int val = 0;
 		int lim = 25;
@@ -52,26 +50,23 @@ int main(){_
 				}
 		}
 		
-		for(int j = lim - 1; j >= 0; j--){
-				if(factor[j] != 0){
-						val = j;
-						break;
-					}
-		}
-					 
-		printf("%3d! =", n);
-		for(int i = 0; i <= val+1; i++){
-				if(i != 0 && i % 15 == 0){
-					printf("\n");
-				}
-					printf("%3d", factor[i]);
+			for(int j = lim - 1; j >= 0; j--){
+					if(factor[j] != 0){
+							val = j;
+							break;
+						}
 			}
-			
-			printf("\n");
+					 
+			printf("%d! =", n);
 
-		}
-		
-		
+			for(int i = 0; i <= val; i++){
+					printf(" %d", factor[i]);
+			}
+	
+		printf("\n");		
+
+	}
+
   return 0;
 }
 
